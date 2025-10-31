405ce3eda660e970a38a450f3107a11dbb98ee68
c19yamamoto <c19yamamoto@gmail.com>
2025-10-31 11:05:02 +0900

    :seedling:create: init

---
+++ test/aws-cdk-issue-35872.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkIssue35872 from '../lib/aws-cdk-issue-35872-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-issue-35872-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkIssue35872.AwsCdkIssue35872Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
